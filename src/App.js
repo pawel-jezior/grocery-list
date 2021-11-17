603637f3b80da8ba49697547a08bebb2d300a935
Pawel Jezior <paweljfrontend@gmail.com>
2021-11-17 16:04:03 +0100

    fun: saving and getting from localStorage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,55 @@
+import React from "react";
+import ProductList from "./components/ProductList";
 
+const App = () => {
+  const textInput = React.createRef();
 
-const productList= []
+  //todo: dorobic przycisk do czyszczenia listy produktow
+  //todo: przy kazdym produkcie przycisk do usuwania go
+  //todo: klikniecie na produkt zaznacza, ze jest zielony lub bialy, domyslnie bialy (oznacza, ze jeszcze nie kupione)
+  //todo: automatyczne usuwanie duplikatow ze storage
 
-function App() {
+  const addNewProductToStorage = (e) => {
+    if (textInput.current.value !== "") {
+      if (getProductListFromStorage()) {
+        const productListFromStorage = getProductListFromStorage().split(",");
+        productListFromStorage.push(textInput.current.value);
+        saveProductListToStorage(productListFromStorage);
+      } else {
+        saveProductListToStorage(textInput.current.value);
+      }
+      clearInput();
+    }
+  };
 
+  const clearInput = () => {
+    textInput.current.value = "";
+  };
 
+  const saveProductListToStorage = (productFronInput) => {
+    localStorage.setItem("productList", productFronInput);
+  };
+
+  const getProductListFromStorage = () => {
+    return localStorage.getItem("productList");
+  };
 
   return (
-    <div className="App">
-      <h1>Lista zakupów</h1>
+    <div className="app">
+      <form className="app__enterProduct">
+        <input ref={textInput} type="text" placeholder="Wprowadź produkt" />
+      </form>
+      <button
+        className="app__submitProduct"
+        onClick={() => {
+          addNewProductToStorage();
+        }}
+      >
+        SUBMIT
+      </button>
+      <ProductList/>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/ProductList.js
@@ -0,0 +1,16 @@
+let producListFromStorageArray = [];
+if (localStorage.getItem("productList")) {
+  producListFromStorageArray = localStorage.getItem("productList").split(",")
+  var productList = producListFromStorageArray.map((product, index) =>
+  <li className="productListContainter__list--element" key={index}>{product}</li>
+)}
+
+const Product = () => {
+  return (
+    <div className="productListContainter">
+      <ul className="productListContainter__list">{productList}</ul>
+    </div>
+  );
+};
+
+export default Product;
